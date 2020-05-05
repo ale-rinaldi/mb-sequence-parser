699b5ea6ce0537a99cb3312b3c3f30cb28795d1a
Alessandro Rinaldi <ale.rinaldi@criptalia.it>
2020-05-05 23:58:54 +0200

    1.1.2: add utility functions to calendar

--- src/Calendar.ts
+++ src/Calendar.ts
@@ -1,3 +1,6 @@
+var Holidays = require("date-holidays");
+var hd = new Holidays("IT");
+
 enum Day {
   Sunday,
   Monday,
@@ -23,6 +26,19 @@ enum Month {
   December,
 }
 
+const Holidayys = [
+  new Date("1970-01-01T00:00:00"),
+  new Date("1970-01-06T00:00:00"),
+  new Date("1970-04-25T00:00:00"),
+  new Date("1970-05-01T00:00:00"),
+  new Date("1970-06-02T00:00:00"),
+  new Date("1970-08-15T00:00:00"),
+  new Date("1970-11-01T00:00:00"),
+  new Date("1970-12-08T00:00:00"),
+  new Date("1970-12-25T00:00:00"),
+  new Date("1970-12-26T00:00:00"),
+];
+
 class Calendar {
   public days: Day[] = [];
   public months: Month[] = [];
@@ -34,6 +50,23 @@ class Calendar {
   public holidays: boolean = false;
   public startDate: Date = new Date(0);
   public endDate: Date = new Date(0);
+
+  private trueIfEqualsOrFirst(first: boolean, second: boolean) {
+    if (first === second) {
+      return true;
+    }
+    return first;
+  }
+
+  public static getWeekNumber(d: Date): number {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    var weekNo = Math.ceil(
+      ((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7
+    );
+    return weekNo;
+  }
 
   public isStartedOn(date: Date): boolean {
     // If the event has not start date, it's always started
@@ -53,6 +86,58 @@ class Calendar {
 
   public isStartedAndNotEndedOn(date: Date): boolean {
     return this.isStartedOn(date) && !this.isEndedOn(date);
+  }
+
+  public isEnabledDayOn(date: Date): boolean {
+    return this.days.indexOf(date.getDay()) > -1;
+  }
+
+  public isEnabledMonthOn(date: Date): boolean {
+    return this.months.indexOf(date.getMonth()) > -1;
+  }
+
+  public onEvenDays() {
+    return this.trueIfEqualsOrFirst(this.evenDays, this.oddDays);
+  }
+
+  public onOddDays() {
+    return this.trueIfEqualsOrFirst(this.oddDays, this.evenDays);
+  }
+
+  public onEvenWeeks() {
+    return this.trueIfEqualsOrFirst(this.evenWeeks, this.oddWeeks);
+  }
+
+  public onOddWeeks() {
+    return this.trueIfEqualsOrFirst(this.oddWeeks, this.evenWeeks);
+  }
+
+  public onWeekDays() {
+    return this.trueIfEqualsOrFirst(this.weekDays, this.holidays);
+  }
+
+  public onHolidays() {
+    return this.trueIfEqualsOrFirst(this.holidays, this.weekDays);
+  }
+
+  public static isHolidayOn(date: Date) {
+    if ([0, 6].indexOf(date.getDay()) > -1) {
+      return true;
+    }
+    return !!hd.isHoliday(date);
+  }
+
+  public isRightDayOn(date: Date) {
+    return (
+      this.isStartedAndNotEndedOn(date) &&
+      this.isEnabledDayOn(date) &&
+      this.isEnabledMonthOn(date) &&
+      (date.getDate() % 2 === 0 ? this.onEvenDays() : this.onOddDays()) &&
+      (Calendar.getWeekNumber(date) % 2 === 0
+        ? this.onEvenWeeks()
+        : this.onOddWeeks()) &&
+      (Calendar.isHolidayOn(date) ? this.onHolidays() : this.onWeekDays())
+    );
   }
 }
 
--- src/__tests__/Calendar.test.ts
+++ src/__tests__/Calendar.test.ts
@@ -1,7 +1,7 @@
-import { Calendar } from "../Calendar";
+import { Calendar, Day, Month } from "../Calendar";
 
-const emptyCalendar = new Calendar();
-const startEndDate = new Calendar();
+let emptyCalendar = new Calendar();
+let startEndDate = new Calendar();
 startEndDate.startDate = new Date("2020-05-04T00:00:00");
 startEndDate.endDate = new Date("2020-05-18T00:00:00");
 
@@ -23,4 +23,151 @@ test("Calendar is ended on", () => {
   expect(startEndDate.isEndedOn(new Date("2020-05-19T00:00:00"))).toBe(true);
   expect(startEndDate.isEndedOn(new Date("2020-05-19T14:23:42"))).toBe(true);
   expect(startEndDate.isEndedOn(new Date("2020-07-14T15:42:16"))).toBe(true);
+});
+
+test("On odd days", () => {
+  const calendar = new Calendar();
+  expect(calendar.onOddDays()).toBe(true);
+  calendar.evenDays = true;
+  expect(calendar.onOddDays()).toBe(false);
+  calendar.oddDays = true;
+  expect(calendar.onOddDays()).toBe(true);
+  calendar.evenDays = false;
+  expect(calendar.onOddDays()).toBe(true);
+});
+
+test("On even days", () => {
+  const calendar = new Calendar();
+  expect(calendar.onEvenDays()).toBe(true);
+  calendar.oddDays = true;
+  expect(calendar.onEvenDays()).toBe(false);
+  calendar.evenDays = true;
+  expect(calendar.onEvenDays()).toBe(true);
+  calendar.oddDays = false;
+  expect(calendar.onEvenDays()).toBe(true);
+});
+
+test("On odd weeks", () => {
+  const calendar = new Calendar();
+  expect(calendar.onOddWeeks()).toBe(true);
+  calendar.evenWeeks = true;
+  expect(calendar.onOddWeeks()).toBe(false);
+  calendar.oddWeeks = true;
+  expect(calendar.onOddWeeks()).toBe(true);
+  calendar.evenWeeks = false;
+  expect(calendar.onOddWeeks()).toBe(true);
+});
+
+test("On even days", () => {
+  const calendar = new Calendar();
+  expect(calendar.onEvenWeeks()).toBe(true);
+  calendar.oddWeeks = true;
+  expect(calendar.onEvenWeeks()).toBe(false);
+  calendar.evenWeeks = true;
+  expect(calendar.onEvenWeeks()).toBe(true);
+  calendar.oddWeeks = false;
+  expect(calendar.onEvenWeeks()).toBe(true);
+});
+
+test("On week days", () => {
+  const calendar = new Calendar();
+  expect(calendar.onWeekDays()).toBe(true);
+  calendar.holidays = true;
+  expect(calendar.onWeekDays()).toBe(false);
+  calendar.weekDays = true;
+  expect(calendar.onWeekDays()).toBe(true);
+  calendar.holidays = false;
+  expect(calendar.onWeekDays()).toBe(true);
+});
+
+test("On holidays", () => {
+  const calendar = new Calendar();
+  expect(calendar.onHolidays()).toBe(true);
+  calendar.weekDays = true;
+  expect(calendar.onHolidays()).toBe(false);
+  calendar.holidays = true;
+  expect(calendar.onHolidays()).toBe(true);
+  calendar.weekDays = false;
+  expect(calendar.onHolidays()).toBe(true);
+});
+
+test("Is holiday on", () => {
+  // Ultimo dell'anno (non festivo)
+  expect(Calendar.isHolidayOn(new Date("2015-12-31T23:59:59"))).toBe(false);
+  // Capodanno (festivo)
+  expect(Calendar.isHolidayOn(new Date("2016-01-01T00:00:00"))).toBe(true);
+  // 25 aprile (festivo)
+  expect(Calendar.isHolidayOn(new Date("2014-04-25T15:23:42"))).toBe(true);
+  // 5 maggio (non festivo)
+  expect(Calendar.isHolidayOn(new Date("2020-05-05T15:23:42"))).toBe(false);
+  // 19 aprile (domenica)
+  expect(Calendar.isHolidayOn(new Date("2020-04-19T15:23:42"))).toBe(true);
+  // 25 aprile (domenica, festivo)
+  expect(Calendar.isHolidayOn(new Date("2020-04-25T23:23:42"))).toBe(true);
+  // Pasquetta (festivo)
+  expect(Calendar.isHolidayOn(new Date("2014-04-21T15:42:16"))).toBe(true);
+});
+
+test("Get week number", () => {
+  expect(Calendar.getWeekNumber(new Date("2015-12-31T23:59:59"))).toBe(53);
+  // First days of year belong to the week of the previous one
+  expect(Calendar.getWeekNumber(new Date("2016-01-01T00:00:00"))).toBe(53);
+  expect(Calendar.getWeekNumber(new Date("2016-01-02T00:00:00"))).toBe(53);
+  // From next monday, it should be week 1
+  expect(Calendar.getWeekNumber(new Date("2016-01-03T23:59:59"))).toBe(53);
+  expect(Calendar.getWeekNumber(new Date("2016-01-04T00:00:00"))).toBe(1);
+  expect(Calendar.getWeekNumber(new Date("2018-07-15T00:00:00"))).toBe(28);
+});
+
+test("Is enabled day on", () => {
+  let saturdayAndWednesday = new Calendar();
+  saturdayAndWednesday.days = [Day.Wednesday, Day.Saturday];
+  expect(
+    saturdayAndWednesday.isEnabledDayOn(new Date("2016-06-18T15:24:31"))
+  ).toBe(true);
+  expect(
+    saturdayAndWednesday.isEnabledDayOn(new Date("2016-06-18T23:59:59"))
+  ).toBe(true);
+  expect(
+    saturdayAndWednesday.isEnabledDayOn(new Date("2016-06-19T00:00:00"))
+  ).toBe(false);
+  expect(
+    saturdayAndWednesday.isEnabledDayOn(new Date("2016-05-11T06:41:03"))
+  ).toBe(true);
+  expect(
+    saturdayAndWednesday.isEnabledDayOn(new Date("2020-05-06T13:23:11"))
+  ).toBe(true);
+});
+
+test("Is enabled month on", () => {
+  let aprilAndNovember = new Calendar();
+  aprilAndNovember.months = [Month.April, Month.November];
+  expect(
+    aprilAndNovember.isEnabledMonthOn(new Date("2010-06-18T15:24:31"))
+  ).toBe(false);
+  expect(
+    aprilAndNovember.isEnabledMonthOn(new Date("2016-11-18T23:59:59"))
+  ).toBe(true);
+  expect(
+    aprilAndNovember.isEnabledMonthOn(new Date("2013-05-19T00:00:00"))
+  ).toBe(false);
+  expect(
+    aprilAndNovember.isEnabledMonthOn(new Date("2015-05-11T06:41:03"))
+  ).toBe(false);
+  expect(
+    aprilAndNovember.isEnabledMonthOn(new Date("2020-04-06T13:23:11"))
+  ).toBe(true);
+});
+
+test("Is right day on", () => {
+  let calendar = new Calendar();
+  calendar.days = [Day.Sunday, Day.Thursday, Day.Friday];
+  calendar.months = [Month.January, Month.October];
+  calendar.oddDays = true;
+  calendar.evenWeeks = true;
+  calendar.weekDays = true;
+  calendar.startDate = new Date("2020-03-21T00:00:00");
+  calendar.endDate = new Date("2021-05-11T00:00:00");
+  expect(calendar.isRightDayOn(new Date("2020-10-15T00:00:00"))).toBe(true);
+  expect(calendar.isRightDayOn(new Date("2020-10-16T00:00:00"))).toBe(false);
 });
